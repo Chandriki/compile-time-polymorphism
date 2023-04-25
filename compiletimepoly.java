9dc4a2da6da7ea76c5063a434464e3e0cfa00326
Chandriki <120454846+Chandriki@users.noreply.github.com>
2023-04-25 10:57:00 -0700

    Add files via upload

---
+++ compiletimepoly.java
@@ -0,0 +1,18 @@
+package chandriki1;
+
+public class compiletimepoly {
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+	public static double add(double a,double b)
+	{
+	return a+b;
+	}
+public static void main(String args[])
+{
+	System.out.println(add(2,3));
+	System.out.println(add(2.0,3.0));
+}
+}
+
